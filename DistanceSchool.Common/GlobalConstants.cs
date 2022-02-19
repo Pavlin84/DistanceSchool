4d67f4187a41a862715539f10b5eb747b31bab88
DELL <pvs.k.brod@gmail.com>
2022-02-19 13:27:47 +0200

    test2

--- DistanceSchool.Common/GlobalConstants.cs
+++ DistanceSchool.Common/GlobalConstants.cs
@@ -33,7 +33,7 @@
         public const string CyrillicMangerCandicdacyHedarMessage = "Кандидатура за директор на";
         public const string CyrillicTeachererCandicdacyHedarMessage = "Кандидатура за учител в";
         public const string CyrillicStudentCandicdacyHedarMessage = "Кандидатура за обучение в";
-        public const string Test = "test";
+        
 
 
     }
